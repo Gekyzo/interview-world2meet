0f9b20a7b17cc8ff3db046dc007a174d25bec30d
Ciro Mora <ciro.mora@gmail.com>
2024-10-17 15:07:03 +0200

    Add webserver

--- src/main/java/com/excelia/spaceships/Application.java
+++ src/main/java/com/excelia/spaceships/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpaceshipsApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpaceshipsApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
