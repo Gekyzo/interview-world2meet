551573a27f9067cc485130b1102318401c1f1cd0
Ciro Mora <ciro.mora@gmail.com>
2024-10-17 13:12:26 +0200

    Initial commit

---
+++ src/test/java/com/excelia/spaceships/SpaceshipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excelia.spaceships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceshipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
