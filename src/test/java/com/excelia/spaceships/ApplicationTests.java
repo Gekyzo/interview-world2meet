f506da71416ef21538464449c8c6c3d84d10bdcb
Ciro Mora <ciro.mora@gmail.com>
2024-10-18 16:14:45 +0200

    Fix sanity test

--- src/test/java/com/excelia/spaceships/ApplicationTests.java
+++ src/test/java/com/excelia/spaceships/ApplicationTests.java
@@ -2,12 +2,17 @@ package com.excelia.spaceships;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads(ApplicationContext context) {
+        // Sanity check test that will fail if the application context cannot start
+        assertThat(context).isNotNull();
     }
 
 }
--- src/test/java/com/excelia/spaceships/infrastructure/in/rest/adapter/post/CreateSpaceshipControllerAdapterTest.java
+++ src/test/java/com/excelia/spaceships/infrastructure/in/rest/adapter/post/CreateSpaceshipControllerAdapterTest.java
@@ -18,7 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Import({CreateSpaceshipRestMapper.class})
-@WebMvcTest(controllers = {CreateSpaceshipController.class})
+@WebMvcTest(controllers = {CreateSpaceshipControllerAdapter.class})
 class CreateSpaceshipControllerAdapterTest extends ControllerTest {
 
     @MockBean
