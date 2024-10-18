c619a84af89eb387487b25a9a4dc728697e06628
Ciro Mora <ciro.mora@gmail.com>
2024-10-18 18:09:44 +0200

    Add correct examples on request

--- src/main/java/com/excelia/spaceships/infrastructure/in/rest/adapter/post/in/CreateSpaceshipRequest.java
+++ src/main/java/com/excelia/spaceships/infrastructure/in/rest/adapter/post/in/CreateSpaceshipRequest.java
@@ -1,6 +1,7 @@
 package com.excelia.spaceships.infrastructure.in.rest.adapter.post.in;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 
@@ -13,10 +14,12 @@ public record CreateSpaceshipRequest(
         @JsonProperty("captain_name")
         String captainName,
 
+        @Schema(example = "100.5")
         @Positive
         Double length,
 
         @Positive
+        @Schema(example = "300000")
         @JsonProperty("max_speed")
         Integer maxSpeed,
 
