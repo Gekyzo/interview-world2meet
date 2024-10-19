d48cf2f0a5f7225ab24202b80f30b72bbb55aa02
Ciro Mora <ciro.mora@gmail.com>
2024-10-20 00:46:21 +0200

    Optimize aspect internal code

--- src/main/java/com/excelia/spaceships/shared/LogNegativeSpaceshipIdAspect.java
+++ src/main/java/com/excelia/spaceships/shared/LogNegativeSpaceshipIdAspect.java
@@ -3,7 +3,6 @@ package com.excelia.spaceships.shared;
 import static com.excelia.spaceships.shared.MessageUtils.getMessageSource;
 
 import jakarta.servlet.http.HttpServletRequest;
-import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import lombok.extern.slf4j.Slf4j;
@@ -19,26 +18,26 @@ import org.springframework.stereotype.Component;
 public class LogNegativeSpaceshipIdAspect {
 
     private static final String MESSAGE_KEY = "errors.spaceship.negativeid";
+    private static final Pattern NEGATIVE_SPACESHIP_ID_PATTERN = Pattern.compile("/spaceships/(-\\d+)");
 
     @Before("execution(* com.excelia.spaceships.shared.FirstFilter.doFilter(..))")
     public void aroundDoFilter(JoinPoint joinPoint) {
         Object[] args = joinPoint.getArgs();
         HttpServletRequest request = (HttpServletRequest) args[0];
+        Matcher matcher = NEGATIVE_SPACESHIP_ID_PATTERN.matcher(request.getRequestURI());
 
-        checkNegativeSpaceshipIdHasBeenRequested(request)
-            .ifPresent(value -> log.info(getMessageSource(MESSAGE_KEY, new Object[]{value})));
-    }
-
-    private Optional<String> checkNegativeSpaceshipIdHasBeenRequested(HttpServletRequest request) {
-        String requestUri = request.getRequestURI();
-        Pattern pattern = Pattern.compile("/spaceships/(.*)");
-        Matcher matcher = pattern.matcher(requestUri);
-
-        if (HttpMethod.GET.matches(request.getMethod()) && matcher.matches()) {
-            return Optional.of(matcher.group(1));
+        if (!HttpMethod.GET.matches(request.getMethod())) {
+            return;
         }
 
-        return Optional.empty();
+        if (!matcher.matches()) {
+            return;
+        }
+
+        int requestedSpaceshipId = Integer.parseInt(matcher.group(1));
+        if (requestedSpaceshipId < 0) {
+            log.info(getMessageSource(MESSAGE_KEY, new Object[]{requestedSpaceshipId}));
+        }
     }
 
 }
