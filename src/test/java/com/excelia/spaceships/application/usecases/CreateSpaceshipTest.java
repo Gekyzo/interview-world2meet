1fb9d03e6c6cc7e972b5409148c975ed52e1fa35
Ciro Mora <ciro.mora@gmail.com>
2024-10-21 13:16:42 +0200

    Remove unnecesary tests

--- src/test/java/com/excelia/spaceships/application/usecases/CreateSpaceshipTest.java
+++ src/test/java/com/excelia/spaceships/application/usecases/CreateSpaceshipTest.java
@@ -1,19 +1,15 @@
 package com.excelia.spaceships.application.usecases;
 
-import static com.junit.object_mothers.SpaceshipObjectMother.aSpaceship;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
 import com.excelia.spaceships.application.mappers.CreateSpaceshipMapper;
 import com.excelia.spaceships.domain.commands.CreateSpaceshipCommand;
-import com.excelia.spaceships.domain.entities.Spaceship;
 import com.excelia.spaceships.domain.events.CreateSpaceshipEvent;
 import com.excelia.spaceships.domain.ports.out.SpaceshipRepositoryPort;
 import com.excelia.spaceships.infrastructure.out.messaging.EventPublisher;
 import org.instancio.Instancio;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -36,27 +32,6 @@ class CreateSpaceshipTest {
     @BeforeEach
     void setUp() {
         this.sut = new CreateSpaceship(repository, mapper, eventPublisher);
-    }
-
-    @Test
-    @Disabled
-    void given_ValidCreateSpaceshipCommand_when_UseCaseIsInvoked_then_MapperIsInvoked() {
-        var command = Instancio.of(CreateSpaceshipCommand.class).create();
-
-        sut.create(command);
-
-        // verify(mapper).toEntity(command);
-    }
-
-    @Test
-    @Disabled
-    void given_ValidCreateSpaceshipCommand_when_UseCaseIsInvoked_then_RepositoryIsInvoked() {
-        given(mapper.toEntity(any())).willReturn(aSpaceship());
-        var command = Instancio.of(CreateSpaceshipCommand.class).create();
-
-        sut.create(command);
-
-        verify(repository).create(any(Spaceship.class));
     }
 
     @Test
